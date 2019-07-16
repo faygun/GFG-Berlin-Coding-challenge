5c7eb985dc4c669ff108bc69fa9077e5676edb14
DESKTOP-5GLTOPK\faygu <faygun89@gmail.com>
2019-07-16 21:08:32 +0300

    created route process.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Login from './component/Login';
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Setting from './component/Setting';
+import Order from './component/Order';
+import Product from './component/Product';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/setting" exact component={Setting} />
+          <Route path="/order" exact component={Order} />
+          <Route path="/product" exact component={Product} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
